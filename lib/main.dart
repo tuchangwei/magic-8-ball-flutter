7b94595cd27e4a63f1dd43802a0445c8f8905e5a
Changwei Tu <changweitu@gmail.com>
2020-04-13 22:43:30 +0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade600,
+            title: Text("Ask me anything"),
+          ),
+          body: MagicBallPage(),
+        ),
       ),
     );
+
+class MagicBallPage extends StatefulWidget {
+  @override
+  _MagicBallPageState createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int randomInt = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+          child: Image.asset('images/ball$randomInt.png'),
+          onPressed: () {
+            setState(() {
+              randomInt = Random().nextInt(5) + 1;
+            });
+          }),
+    );
+  }
+}
